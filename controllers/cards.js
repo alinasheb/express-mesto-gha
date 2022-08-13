af2f6daf14e47d7d6b61436277b38b9b020639a0
Alina Shebalkina <farmazonka1994@yandex.ru>
2022-08-13 22:47:08 +0300

    13 проект

--- controllers/cards.js
+++ controllers/cards.js
@@ -42,7 +42,7 @@ const deleteCard = (req, res) => {
 const likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { $addToSet: { likes: req.user._id } }, 
     { new: true },
   )
     .orFail(() => {
@@ -64,7 +64,7 @@ const likeCard = (req, res) => {
 const disLikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { $pull: { likes: req.user._id } },
     { new: true },
   )
     .orFail(() => {
